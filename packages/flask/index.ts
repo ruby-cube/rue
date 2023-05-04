58a6efb2872d33da16f11182e02adff4e8c31a75
Ruby Wang <ruby.yiwang@gmail.com>
2023-05-03 21:48:02 -0700

    move genTargetID to Utils

--- packages/flask/index.ts
+++ packages/flask/index.ts
@@ -1,6 +1,5 @@
 export { $listen, $schedule, $subscribe, $lifetime, $tilStop, $outlive, initAutoCleanup } from "./flaskedListeners"
 export { initFlask } from "./initFlask"
-export { genTargetID } from "./targetID"
 export {inSceneSetup} from "./Scene"
 export type * from "./flaskedListeners"
 export type * from "./dev-utils"
